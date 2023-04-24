1687e928b02f72c5d727eb0627d24e811fbb8472
snorlax19 <74537035+snorlax19@users.noreply.github.com>
2023-04-24 19:50:46 +0200

    Updates

---
+++ precache-manifest.b6d4cd3f992da88c6209847068e4625d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97392f344b0a921a5cc8",
+    "url": "/static/css/main.e15d49c9.chunk.css"
+  },
+  {
+    "revision": "97392f344b0a921a5cc8",
+    "url": "/static/js/main.97392f34.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "f286f40f0a51d5858df01492f9606103",
+    "url": "/index.html"
+  }
+];
